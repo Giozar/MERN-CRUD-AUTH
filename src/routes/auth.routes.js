5748993f67c322d8ebcebbb6a5423ca21af55f4a
giovanni <manuelgio.cortazar.delacruz@gmail.com>
2023-07-01 15:55:58 -0600

    Implemetación de Task CRUD y validación de datos

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,17 @@
 import {Router} from 'express'
 import { login, register, logout, profile } from '../controllers/auth.controller.js';
 import { authRequire } from '../middlewares/validateToken.js';
+import { validateSchema } from '../middlewares/validator.middlewares.js';
+import { loginSchema, registerSchema } from '../schemas/auth.schemas.js';
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login); 
-router.post('/logout', logout); 
-router.get('/profile', authRequire, profile ); 
+router.post("/register", validateSchema(registerSchema), register);
+
+router.post("/login", validateSchema(loginSchema), login); 
+
+router.post("/logout", logout); 
+
+router.get("/profile", authRequire, profile ); 
 
 export default router
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,8 +1,15 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import { authRequire } from '../middlewares/validateToken.js';
+import { getTasks, getTask, creatTask, updateTask, deleteTask } from '../controllers/tasks.controller.js';
+import { validateSchema } from '../middlewares/validator.middlewares.js';
+import { createTaskSchema } from '../schemas/task.schemas.js';
 
 const router = Router();
 
-router.get('/tasks', authRequire, (req, res) => res.send('tasks'));
+router.get('/tasks', authRequire, getTasks);
+router.get('/tasks/:id', authRequire, getTask);
+router.post('/tasks', authRequire, validateSchema(createTaskSchema), creatTask);
+router.delete('/tasks/:id', authRequire, deleteTask);
+router.put('/tasks/:id', authRequire, updateTask);
 
 export default router;
